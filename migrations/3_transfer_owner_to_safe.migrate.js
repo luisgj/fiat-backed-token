502a4560611fc0b8743f2aed0cdbd9743bf57ffc
luisgj <guayabas@hey.com>
2021-06-21 00:38:57 -0500

    add simple test suite.

--- migrations/3_transfer_owner_to_safe.migrate.js
+++ migrations/3_transfer_owner_to_safe.migrate.js
@@ -8,7 +8,7 @@ module.exports = async function (deployer, network) {
   const gnosisSafe = '0xD476Dcf0453b49130Fe5D4820cDeaa1679D225Bf';
  
   // Don't change ProxyAdmin ownership for our test network
-  if (network !== 'mainnet') {
+  if (network === 'rinkeby') {
     // The owner of the ProxyAdmin can upgrade our contracts
     await admin.transferProxyAdminOwnership(gnosisSafe);
   }
